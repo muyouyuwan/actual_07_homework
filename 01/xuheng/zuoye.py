205623b712ef82c9a8207e77a73265c5ce3c9559
XuHeng <xh0628@foxmail.com>
2015-12-10 10:43:41 +0800

    xuheng@1210

--- 01/xuheng/zuoye.py
+++ 01/xuheng/zuoye.py
@@ -27,20 +27,5 @@ for i in list_b:
 print 'num %s,%s' % (max_a,max_b)
 
 
-#! /usr/bin/python
-# Filename : table_9x9.py
-# Author : Jesse
-# Date : 2011/08/13 21:50
-
-print '\n9x9 Table\n'
-
-for i in range(1, 10) :
-    for j in range(1, i+1) :
-        print j, 'x', i, '=', j*i, '\t',
-        # print '%d x %d = %d\t' %(j, i, j*i),
-    print '\n'
-
-print '\nDone!'
-
 
 
