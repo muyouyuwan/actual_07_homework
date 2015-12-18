2bd235dafb715743aba7ceebd87ad7b8985f6f06
xxrenzhe <yj2008ay611@gmail.com>
2015-12-18 16:32:48 +0800

    modiy zuoye.py
    增加选择排序和快速排序实现

--- 02/yujie/zuoye.py
+++ 02/yujie/zuoye.py
@@ -13,7 +13,7 @@
 # 6. 重复步骤2~5
 # 参考资料: http://bubkoo.com/2014/01/14/sort-algorithm/insertion-sort/
 
-# solution 1
+# 插入排序方法一
 def insert_sort(my_list):
     print "The original list is %s" % my_list
 
@@ -29,3 +29,92 @@ def insert_sort(my_list):
 print "="*50+"solution 1"+"="*50
 List1 = [12,3,1,21,2,2,3,4111,22,3333,45,33,45]
 insert_sort(List1)
+
+# 插入排序方法二
+def insert_sort_2(my_list):
+    print "The original list is %s" % my_list
+    loop_count = 0
+
+    for i in range(1,len(my_list)):
+        start = i
+        end = 0
+        step = -1
+        for idx in range(start,end,step):
+            if my_list[idx] < my_list[idx-1]:
+                my_list[idx],my_list[idx-1] = my_list[idx-1],my_list[idx]
+                loop_count += 1
+            else:
+                break
+
+    print "After insert sort,the list is %s" % my_list
+    print "After insert sort,the loop count is %d" % loop_count
+
+print "="*50+"solution 2 insert sort 2"+"="*50
+List1 = [12,3,1,21,2,2,3,4111,22,3333,45,33,45]
+insert_sort_2(List1)
+
+# 选择排序(selection sort)
+def selection_sort(my_list):
+    print "The original list is %s" % my_list
+    loop_count = 0
+    temp_list = []
+
+    for i in range(0,len(my_list)):
+        list_min = min(my_list)
+        temp_list.append(list_min)
+        my_list.remove(list_min)
+        loop_count += 1
+
+    temp_list,my_list = my_list,temp_list
+    print "After selection sort,the list is %s" % my_list
+    print "After selection sort,the loop count is %d" % loop_count
+
+print "="*50+"solution 3 selection sort"+"="*50
+List1 = [12,3,1,21,2,2,3,4111,22,3333,45,33,45]
+selection_sort(List1)
+
+# 快速排序
+def quick_sort(my_list,left,right):
+    if left >= right or right < 2:
+        return 1
+
+    global loop_count_2
+    storeindex = left
+    pivot = my_list[right]
+
+    for k in range(left,right):
+        if my_list[k] < pivot:
+            if k != storeindex:
+                my_list[k],my_list[storeindex] = my_list[storeindex],my_list[k]
+                loop_count_2 += 1
+            storeindex += 1
+    my_list[right],my_list[storeindex] = my_list[storeindex],my_list[right]
+
+    quick_sort(my_list,left,storeindex-1)
+    quick_sort(my_list,storeindex+1,right)
+
+print "="*50+"solution 4 quick sort"+"="*50
+loop_count_2 = 0
+List1 = [12,3,1,21,2,2,3,4111,22,3333,45,33,45]
+print "The original list is %s" % List1
+quick_sort(List1,0,len(List1)-1)
+print "After insert sort,the list is %s" % List1
+print "After insert sort,the loop count is %d" % loop_count_2
+
+# 各种排序数据交换次数对比结果，即以上程序的执行结果
+==================================================solution 1 insert sort 1==================================================
+The original list is [12, 3, 1, 21, 2, 2, 3, 4111, 22, 3333, 45, 33, 45]
+After insert sort,the list is [1, 2, 2, 3, 3, 12, 21, 22, 33, 45, 45, 3333, 4111]
+After insert sort,the loop count is 20
+==================================================solution 2 insert sort 2==================================================
+The original list is [12, 3, 1, 21, 2, 2, 3, 4111, 22, 3333, 45, 33, 45]
+After insert sort,the list is [1, 2, 2, 3, 3, 12, 21, 22, 33, 45, 45, 3333, 4111]
+After insert sort,the loop count is 20
+==================================================solution 3 selection sort==================================================
+The original list is [12, 3, 1, 21, 2, 2, 3, 4111, 22, 3333, 45, 33, 45]
+After selection sort,the list is [1, 2, 2, 3, 3, 12, 21, 22, 33, 45, 45, 3333, 4111]
+After selection sort,the loop count is 13
+==================================================solution 4 quick sort==================================================
+The original list is [12, 3, 1, 21, 2, 2, 3, 4111, 22, 3333, 45, 33, 45]
+After insert sort,the list is [1, 2, 2, 3, 3, 12, 21, 22, 33, 45, 45, 3333, 4111]
+After insert sort,the loop count is 5
