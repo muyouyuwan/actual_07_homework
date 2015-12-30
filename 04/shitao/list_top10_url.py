cff0b12a14a67b820ad0eef9b23b0f2dfadd9db3
siitao <siitao@users.noreply.github.com>
2015-12-30 17:35:45 +0800

    add the homework 4

---
+++ 04/shitao/list_top10_url.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#coding=utf-8
+file_name = "www_access_20140823.log"
+_dict = {}
+try:
+	# 把日志从文件中读取出来以字典的形式储存 便于后面处理
+	_file = open(file_name,'r')
+	for line in _file:
+		full_url = line.split()[8],line.split()[6],line.split()[0]
+		_dict.setdefault(full_url,0)
+		_dict[full_url] += 1
+except BaseException:
+	print "There have been some mistake maybe the %s is not exist " % file_name
+else:
+	# 比较大小 列出排名前十的日志 并按指定格式展示
+ 	_dict = _dict.items()
+	if len(_dict) -1 > 10:
+		sort_len = 10
+	for j in range(sort_len):
+		for i in range(len(_dict) -1):
+			if _dict[i][1] > _dict[i+1][1]:
+				_dict[i],_dict[i+1] = _dict[i+1],_dict[i]
+		res = _dict[(j+1)*-1]
+		_list = [res[0][0],res[0][1],(res[0][-1],res[-1])]
+		print _list
